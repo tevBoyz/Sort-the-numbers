3de6e087a55d2205e28fbb43faaa372d670c4961
Dawit Tefera <69167983+tevBoyz@users.noreply.github.com>
2024-02-10 14:08:49 +0300

    Added popup for instruction

--- script.js
+++ script.js
@@ -8,7 +8,34 @@ let prev = 0;
 
 var trackingArr = [-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1]
 
+  // Get the modal
+  var modal = document.getElementById("myModal");
 
+  // Get the button that opens the modal
+  var btn = document.getElementById("myBtn");
+  
+  // Get the <span> element that closes the modal
+  var span = document.getElementsByClassName("close")[0];
+  
+  // When the user clicks on the button, open the modal
+  function popup() {
+    modal.style.display = "block";
+  }
+  
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function() {
+    modal.style.display = "none";
+  }
+  
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
+
+
+popup();
 
 function start(){
     randomize();
@@ -145,3 +172,7 @@ function animateValue(obj, start, end, duration) {
     };
     window.requestAnimationFrame(step);
   }
+
+
+
+
